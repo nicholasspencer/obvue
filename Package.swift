c133b2845108e41733ce9187326a9ae639fb3638
Nicholas Spencer <nick.switch@gmail.com>
2019-05-30 16:54:04 -0500

    Expose products

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "obvue",
+    products: [
+        .executable(name: "obvue", targets: ["Obvue"]),
+        .library(name: "ObvueCore", targets: ["ObvueCore"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "obvue", dependencies: ["ObvueCore","Commander"]),
+        .target(name: "Obvue", dependencies: ["ObvueCore","Commander"]),
         .target(name: "ObvueCore"),
         .testTarget(name: "ObvueCoreTests", dependencies: ["ObvueCore"]),
     ]
--- Tests/obvueTests/XCTestManifests.swift
+++ Tests/obvueTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(obvueTests.allTests),
+        testCase(ObvueTests.allTests),
     ]
 }
 #endif
